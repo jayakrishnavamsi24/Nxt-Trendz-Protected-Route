e80434e2fd875a8d5b1b7c18a3c3d448793c341d
jayakrishnavamsi24 <jayakrishnavamsi24@gmail.com>
2023-05-26 11:52:17 +0000

    pushed code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Route, Redirect, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route path="/bad-path" component={NotFound} />
+    <Redirect to="/bad-path" />
+  </Switch>
+)
+
+export default App
